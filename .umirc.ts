d77236da2babf694587bec4bd0deee190079ee7e
zhuling904 <ling.zhu.904@gmail.com>
2024-06-14 23:25:02 +0800

    feat: init

--- .umirc.ts
+++ .umirc.ts
@@ -46,8 +46,8 @@ export default defineConfig({
   },
   hash: isProd,
   // 这里用你的仓库名
-  base: isProd ? '/book_template/' : '/', // router base
-  publicPath: isProd ? '/book_template/' : '/', // router base
+  base: isProd ? '/front_end_engineer_book/' : '/', // router base
+  publicPath: isProd ? '/front_end_engineer_book/' : '/', // router base
   headScripts: [
     {
       async: true,
